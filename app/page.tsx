6a31644be06c0365cc0e18c3521902144ff0985c
Gautam <gautamanand931@gmail.com>
2025-11-26 01:03:26 +0530

    feat: initial setup, theme, package installation

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <main className="container mx-auto px-4">
+        {/* Your content here */}
+      </main>
+    </div>
+  );
+}
