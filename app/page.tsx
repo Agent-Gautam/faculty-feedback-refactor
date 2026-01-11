b195612c1d253046f38efe39e4cae2b8b423ad38
Gautam <gautamanand931@gmail.com>
2026-01-11 23:16:08 +0530

    chore: update .gitignore to include docker and modify homepage to indicate ongoing refactoring with a link to Notion

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <main className="container mx-auto px-4">
-        {/* Your content here */}
+      <main className="flex items-center justify-center flex-col gap-4 mx-auto px-4">
+        <h1 className="text-2xl font-bold">Project is under refactoring</h1>
+        <p>
+          Check Progress on{" "}
+          <a
+            className="text-blue-500"
+            href="https://www.notion.so/Faculty-Feedback-System-2b69e771361780f39425dc3a59f745ad?source=copy_link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Notion
+          </a>
+        </p>
       </main>
     </div>
   );
